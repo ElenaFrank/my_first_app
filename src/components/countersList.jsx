3a0019c318b4643f0b83ff320cd06e6f6b7884a8
Alyona <alyonazholob3@gmail.com>
2022-09-13 21:14:42 +0700

    add useState an d Spred  in CountersList.jsx

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Counter from "./counter";
 
 const Counters = () => {
-    const counters = [
+    const [counters, setCounter] = useState([
         {id:0, value:0, name: 'Ненужная вещь'},
         {id:1, value:8, name: 'Вилка'},
         {id:2, value:1, name: 'Ложка'},
         {id:3, value:1, name: 'Тарелка'},
         {id:4, value:1, name: 'Набор минималиста'}
-    ]
+    ])
+
+    const handleDelete = (id) => {
+        console.log('handle')
+        setCounter(counters=>counters.filter(c=> c.id!==id ))
+
+    }
+
     return (
         <>
             {counters.map(count => 
-                <Counter key={count.id} value={count.value} name={count.name}>
+                <Counter
+                    key={count.id}
+                    id={count.id}
+                    value={count.value}
+                    name={count.name}
+                    onDelete={handleDelete}>
                 </Counter>
             )}
         </>
