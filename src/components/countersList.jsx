d9e99c77f5d200676cd45f2549e056464770984e
Alyona <alyonazholob3@gmail.com>
2022-09-14 20:53:03 +0700

    moved handleIncrement and handleDecrement in counterList

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,13 +2,35 @@ import React, {useState} from "react";
 import Counter from "./counter";
 
 const Counters = () => {
-    const [counters, setCounter] = useState([
+
+    const initState = [
         {id:0, value:0, name: 'Ненужная вещь'},
         {id:1, value:8, name: 'Вилка'},
         {id:2, value:1, name: 'Ложка'},
         {id:3, value:1, name: 'Тарелка'},
         {id:4, value:1, name: 'Набор минималиста'}
-    ])
+    ]
+
+    const [counters, setCounter] = useState(initState)
+
+    const handleIncrement = (id) => {
+        setCounter(initState=>initState.map(c => {
+            if (c.id === id) {
+                c.value += 1
+            }
+            return c
+        }))   
+    }
+
+    const handleDecrement = (id) => {
+
+        setCounter(initState=>initState.map(c => {
+            if(c.id===id) {
+                c.value -= 1
+            }
+            return c
+        }))
+    }
 
     const handleDelete = (id) => {
         console.log('handle')
@@ -16,17 +38,24 @@ const Counters = () => {
 
     }
 
+    const handleReset = () => {
+        setCounter(initState)
+        console.log('handleReset')
+    }
+
     return (
         <>
             {counters.map(count => 
                 <Counter
                     key={count.id}
-                    id={count.id}
-                    value={count.value}
-                    name={count.name}
-                    onDelete={handleDelete}>
+                    {...count}
+                    onDelete={handleDelete}
+                    onIncrement={handleIncrement}
+                    onDecrement={handleDecrement}
+                >
                 </Counter>
             )}
+            <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Reset</button>
         </>
     )
 }
